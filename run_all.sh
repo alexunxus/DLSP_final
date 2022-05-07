eeb49859cff4aa3fb714c5a3d7d7871d89746b41
alexunxus <b01401089@ntu.edu.tw>
2022-05-07 02:23:27 -0400

    modify runall

--- run_all.sh
+++ run_all.sh
@@ -1,15 +1,15 @@
 python3 inference.py --norm clean >> ./weight/clean_default.txt
 python3 inference.py --task SSL --norm clean
 python3 inference.py --norm l_1 >> ./weight/l_1_default.txt
-python3 inference.py --task SSL --norm l_1 --attack_iter 5
-python3 inference.py --task SSL --norm l_1 --attack_iter 10
-python3 inference.py --task SSL --norm l_1 --attack_iter 15
+python3 inference.py --task SSL --norm l_1 --iter 5
+python3 inference.py --task SSL --norm l_1 --iter 10
+python3 inference.py --task SSL --norm l_1 --iter 15
 python3 inference.py --norm l_2 >> ./weight/l_2_default.txt
-python3 inference.py --task SSL --norm l_2 --attack_iter 5
-python3 inference.py --task SSL --norm l_2 --attack_iter 10
-python3 inference.py --task SSL --norm l_2 --attack_iter 15
+python3 inference.py --task SSL --norm l_2 --iter 5
+python3 inference.py --task SSL --norm l_2 --iter 10
+python3 inference.py --task SSL --norm l_2 --iter 15
 python3 inference.py --norm l_inf >> ./weight/l_inf_default.txt
-python3 inference.py --task SSL --norm l_inf --attack_iter 5
-python3 inference.py --task SSL --norm l_inf --attack_iter 10
-python3 inference.py --task SSL --norm l_inf --attack_iter 15
+python3 inference.py --task SSL --norm l_inf --iter 5
+python3 inference.py --task SSL --norm l_inf --iter 10
+python3 inference.py --task SSL --norm l_inf --iter 15
 
